4954739b0513040f57d1a6bdc9e5a90d21d5a3c7
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2022-10-13 15:18:47 -0300

    Create aula2_12Comentarios.py

---
+++ aula2_12Comentarios.py
@@ -0,0 +1,12 @@
+"""" 
+print('loren ipsun')
+print('linha 2')
+"""""
+#poderia adicionar um comentario falando do que se trata essa linha  de codigo
+#comentario de uma linha ou duas linhas
+print('loren 3')
+print('loren 4')  # pode comentar no fim da linha, lembrando de adicionar dois espaços antes da cerquilha
+print('loren ipsun')  # outro comentario
+print('loren ipsun')  # comentarios obvios tb nao é legal.
+# print('loren ipsun') Pode adicionar na frente do codigo tb
+# final
